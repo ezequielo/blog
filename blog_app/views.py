44b9cff4230b533bc8d85393862f437afc059881
usuario1 <usuario1@GSTD018>
2014-07-18 15:03:57 +0200

    .

--- blog_app/views.py
+++ blog_app/views.py
@@ -12,8 +12,44 @@ def index(request):
 
 def detail(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
+    
+    # recorrer todos los comentarios???? :/ :/
     comments = Comment.objects.filter(
             fk_post=post_id
         ).order_by('pub_date')
     
     return render(request,'blog_app/detail.html',{'post':post, 'comment_list':comments})
+
+
+# def submit_comment(request, post_id):
+#     p = get_object_or_404(Post, pk=post_id)
+#     
+#     if request.POST['comment']:
+#         print(request.POST['comment'])
+#         
+# #     return render(request,'blog_app/detail.html',{'post':post, 'comment_list':comments})
+#     return render(request,'blog_app/detail.html',{})
+    
+    
+    
+    
+    
+# def vote(request, poll_id):
+#     p = get_object_or_404(Poll, pk=poll_id)
+#     try:
+#         selected_choice = p.choice_set.get(pk=request.POST['choice'])
+#     except (KeyError, Choice.DoesNotExist):
+#         # Redisplay the poll voting form.
+#         return render(request, 'polls/detail.html', {
+#             'poll': p,
+#             'error_message': "You didn't select a choice.",
+#         })
+#     else:
+#         selected_choice.votes += 1
+#         selected_choice.save()
+#         # Always return an HttpResponseRedirect after successfully dealing
+#         # with POST data. This prevents data from being posted twice if a
+#         # user hits the Back button.
+#         return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
+    
+    
